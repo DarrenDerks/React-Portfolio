e1189df4f57cbc88c14beb1f31273a81f441055d
Darren Derks <leederksjr@gmail.com>
2020-11-03 22:00:01 -0700

    commit

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -14,6 +14,8 @@ export default class BlogForm extends Component {
       blog_status: "",
       content: "",
       featured_image: "",
+      apiUrl: "https://darrenderks.devcamp.space/portfolio/portfolio_blogs",
+      apiAction: "post",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -48,7 +50,10 @@ export default class BlogForm extends Component {
       this.setState({
         id: this.props.blog.id,
         title: this.props.blog.title,
-        status: this.props.blog.status,
+        blog_status: this.props.blog.blog_status,
+        content: this.props.blog.content,
+        apiUrl: `https://darrenderks.devcamp.space/portfolio/portfolio_blogs/${this.props.blog.id}`,
+        apiAction: "patch",
       });
     }
   }
@@ -95,12 +100,12 @@ export default class BlogForm extends Component {
   }
 
   handleSubmit(event) {
-    axios
-      .post(
-        "https://darrenderks.devcamp.space/portfolio/portfolio_blogs",
-        this.buildForm(),
-        { withCredentials: true }
-      )
+    axios({
+      method: this.state.apiAction,
+      url: this.state.apiUrl,
+      data: this.buildForm(),
+      withCredentials: true,
+    })
       .then((response) => {
         if (this.state.featured_image) {
           this.featuredImageRef.current.dropzone.removeAllFiles();
@@ -112,7 +117,13 @@ export default class BlogForm extends Component {
           featured_image: "",
         });
 
-        this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
+        if (this.props.editMode) {
+          this.props.handleUpdateFormSubmission(response.data.portfolio_blog);
+        } else {
+          this.props.handleSuccessfulFormSubmission(
+            response.data.portfolio_blog
+          );
+        }
       })
       .catch((error) => {
         console.log("handleSubmit for blog error", error);
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,45 @@
 import React from "react";
+import profilePic from "../../../static/assets/images/bio/mypic.jpg";
 
 export default function () {
-  return <div>About</div>;
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePic + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
+      <div className="right-column">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+        officia dicta, suscipit aliquam, est laudantium aut ipsa eos repellat
+        quasi reprehenderit architecto officiis sed vitae delectus commodi nam
+        nostrum animi. Tempora iure aut sapiente sunt minima aperiam quas et
+        quam ullam nesciunt magni nobis, doloribus autem laudantium nisi quos ad
+        perspiciatis velit nemo. Illo, eligendi excepturi autem deleniti
+        quibusdam eaque! Aliquid harum quia et laborum ducimus libero minima
+        vitae enim assumenda, maiores ullam quisquam blanditiis ad non
+        doloremque! Voluptates, suscipit excepturi? Iste minima quos eum sunt,
+        dolorum eius repudiandae blanditiis. Dolore eaque sint ex ut, vero
+        voluptates excepturi, delectus corporis odio temporibus quibusdam soluta
+        officia eveniet pariatur laboriosam veniam minus at harum aliquid? Error
+        eos magnam mollitia repudiandae, in enim. Aperiam nostrum consequuntur
+        veritatis ratione reprehenderit cupiditate autem tempora quisquam
+        consectetur cumque, quae, sequi repellendus, quos facere. Tempore ipsam
+        odio at sequi reprehenderit incidunt soluta? At maxime ratione nulla
+        voluptates? Adipisci quos repellendus placeat nemo eius voluptas sit nam
+        id, ullam voluptatibus nobis eum? Ducimus illum accusamus voluptatibus
+        ad architecto repellat animi commodi dignissimos minima sit cumque,
+        quasi ipsa sint? Iusto esse molestiae consequatur maiores atque, cumque
+        voluptas blanditiis expedita culpa quam vero et commodi sed harum iste
+        at labore, eaque magni repellat provident officia laborum quaerat
+        doloremque nam! Veritatis. Nisi id hic sed voluptas laudantium. Laborum
+        quam adipisci laboriosam reiciendis sit accusantium, ad corrupti sunt
+        consectetur, magnam qui dolore rem necessitatibus. Voluptatem hic ea
+        explicabo vero unde earum porro! Incidunt facilis illo esse velit hic
+      </div>
+    </div>
+  );
 }
