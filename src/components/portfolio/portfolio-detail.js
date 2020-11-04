30f107c9536ac3bfd3171946ace44d2dd8ffea1d
Darren Derks <leederksjr@gmail.com>
2020-11-03 23:17:05 -0700

    commit

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,35 @@
-import React from "react";
+import Axios from "axios";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function (props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  );
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://darrenderks.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log("res", response);
+      })
+      .catch((error) => {
+        console.log("getportfolioitem error", error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      </div>
+    );
+  }
 }
