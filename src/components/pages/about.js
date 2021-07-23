2bcb2bce2b3151dc5d448de7b379e30dd72563c7
Darren Derks <leederksjr@gmail.com>
2021-07-22 19:05:56 -0600

    fixes

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,7 +13,7 @@ export default function () {
         }}
       ></div>
       <div className="right-column">
-      <p>Hi, my name is Darren Derks and I’m a software developer who’s passionate about all thing's computers and computer software. I recently just finished an excelled full stack software engineer program at Bottega University where I gained skills in REACT, JavaScript, Python, MySQL, HTML, CSS, and Sass.</p>
+      <p>Hi, my name is Darren Derks and I’m a software developer who’s passionate about all thing's computers and computer software. I recently just finished an excelled full stack software engineer program at Bottega University where I gained skills in <bold>REACT</bold>, <bold>JavaScript</bold>, <bold>Python</bold>, <bold>MySQL</bold>, <bold>HTML</bold>, <bold>CSS</bold>, and <bold>Sass</bold>.</p>
 
       <p>I’m a developer, because I’m inspired by the idea of making software that makes life easier or more enjoyable for people. Put simply, I love to learn and this portfolio and the projects contained inside are a great example of that. Not only did I use all the skills listed above but I also used other things I chose to learn myself like hooks, Speechly, machine learning, etc.</p>
 
@@ -25,7 +25,7 @@ export default function () {
 
 
       <p>If you’d like to learn more about what makes me tick feel free to visit my contact section.</p>
-      
+
       </div>
     </div>
   );
